302a307f89ad6a97f0d501bbe6c79b21764361bf
nanthitha05252005 <nanthithananthi2525@gmail.com>
2024-12-14 16:00:22 +0530

    Add files via upload

---
+++ doc.js
@@ -0,0 +1,15 @@
+const homeElement=document.getElementById("home")
+homeElement.onclick=function()
+{
+    homeElement.style.backgroundColor="blue"
+}
+const aboutElement=document.getElementById("about")
+aboutElement.onclick=function()
+{
+    aboutElement.style.backgroundColor="blue"
+}
+const servicesElement=document.getElementById("services")
+servicesElement.onclick=function()
+{
+    servicesElement.style.backgroundColor="blue"
+}
